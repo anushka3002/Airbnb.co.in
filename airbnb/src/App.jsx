54da365438d713e23cff37a4f8ec3dfebb1629e7
Anushka <95957335+anushka3002@users.noreply.github.com>
2022-03-29 12:56:52 +0530

    working on navbar

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import {Navbar} from "./Components/Navbar/Navbar"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   )
 }
---
+++ airbnb/src/Components/Navbar/navbar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export const Navbar =()=>{
+    return (
+        <div>
+            <div id="anavbar">
+            {/* <img src="https://starpng.com/public/uploads/preview/airbnb-logo-airbnb-logo-white-on-black-png-image-transparent-101576586899a2xgsawyyh.png" width="150px" height="100px"></img> */}
+            <img src="https://logos-download.com/wp-content/uploads/2016/03/Airbnb_logo.png" width="130px" height="40px"></img>
+            </div>
+        </div>
+    )
+}
