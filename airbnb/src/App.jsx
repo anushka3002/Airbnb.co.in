ed715453b9319174cf8b5d442e2257184f2e33cd
yashsakalley-1997 <sakalley.yash@gmail.com>
2022-04-02 11:33:55 +0530

    changes

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -4,12 +4,8 @@ import './App.css'
 // import {Navbar} from "./Components/Navbar/Navbar"
 import { LandingPage } from './Components/LandingPage/LandingPage'
 import {Footer} from "./Components/Footer/Footer"
-<<<<<<< HEAD
-import { MyFD } from './Components/FlexibleDetails/FlexibleDetails'
-=======
 import { Singup } from './Components/LoginSignup/Signup'
 import { Host } from './Components/Become_a_host/Host'
->>>>>>> 15d833d77b67849b0ed332ac943145ea7a6c5300
 // import { Display } from './Components/Flexible/Display/Display'
 import { Navbar } from "./Components/Flexible/Navbar/Navbar"
 
@@ -19,15 +15,10 @@ function App() {
   return (
     <div className="App">
       {/* <Navbar/> */}
-      {/* <LandingPage/> */}
-<<<<<<< HEAD
-      <MyFD/>
-      <Footer/>
-=======
+      <LandingPage/>
       {/* <Singup/> */}
-      <Host/>
+      {/* <Host/> */}
       {/* <Footer/> */}
->>>>>>> 15d833d77b67849b0ed332ac943145ea7a6c5300
     </div>
   )
 }
