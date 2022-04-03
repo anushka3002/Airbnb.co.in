e6ce17a2115126b107f83db88d6dde952f6f5d93
sivaji <pandasivaji@gmail.com>
2022-04-03 12:49:02 +0530

    routes updated
    updated

--- airbnb/src/Components/Routers/router.jsx
+++ airbnb/src/Components/Routers/router.jsx
@@ -6,7 +6,6 @@ import { Superhost } from "../Superhost/Superhost";
 import {Ukraine} from "../Ukraine/Ukraine";
 import {MyFD} from "../FlexibleDetails/FlexibleDetails"
 
-
 import {Payment} from "../Payment/Payment"
 import { Checkout } from "../Payment/Checkout"
 import { Card } from "../Payment/Card"
@@ -18,11 +17,12 @@ export const Routers=()=>{
         <>
         <Routes>
             <Route path="/" element={<LandingPage/>}/>
-            <Route path="/become_a_host" element={<Host/>}/>
-            <Route path="/flexible" element={<NavbarComponent/>}/>
-            <Route path="/superhost" element={<Superhost/>}/>
-            <Route path="/Support_Ukraine" element={<Ukraine/>}/>
-            <Route path ={"/payment"} element = {<Payment/>}></Route>
+        <Route path="/become_a_host" element={<Host/>}/>
+        <Route path="/flexible" element={<NavbarComponent/>}/>
+        <Route path="/superhost" element={<Superhost/>}/>
+        <Route path="/Support_Ukraine" element={<Ukraine/>}/>
+        <Route path="/details/:id" element={<MyFD/>}/>
+        <Route path="/payment/:id" element = {<Payment/>}></Route>
        <Route path ={"/check"} element = {<Checkout/>}></Route>
        <Route path ={"/card"} element = {<Card/>}></Route>
        <Route path ={"/thank"} element = {<Thank/>}></Route>
